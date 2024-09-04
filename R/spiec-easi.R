621cb60943e88ebc020fb06141e10d600d0b6209
Anton Kjellberg <antonkjellberg@hotmail.com>
2024-09-04 18:39:24 +0200

    Updat package

--- R/spiec-easi.R
+++ R/spiec-easi.R
@@ -57,11 +57,11 @@ spiec.easi.otu_table <- function(data, ...) {
 
 
 #' @noRd
-.spiec.easi.norm <- function(data) {
+.spiec.easi. <- function(data) {
 # internal function to normalize a data matrix
   if (inherits(data, 'matrix')) {
     ## standard data pipeline
-    return(t(clr(data+1, 1)))
+    return(t(data))
   } else if (inherits(data, 'list')) {
     ## multi domain spiec.easi, data must be list of numeric matrices
     return(do.call('cbind', lapply(data, .spiec.easi.norm)))
